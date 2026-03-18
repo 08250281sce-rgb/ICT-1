e4eb43b78d76ab5f90d6f0ae41a057280726ebd1
unknown <08250281.sce@rub.edu.bt>
2026-03-18 09:52:45 +0600

    my frist string coding

---
+++ demo/stringEx.py
@@ -0,0 +1,12 @@
+print("HELLO")
+MultipleLines= '''This is a string
+that spans multiple lines.'''
+print(MultipleLines)
+a=1
+b=2
+c=a+b
+print(c)
+print(type(c)) #c is an integer
+c=str(c) #c is now a string 
+print(type(c)) #c is now a string 
+print(c) #c is now a string that contain the value of 3
