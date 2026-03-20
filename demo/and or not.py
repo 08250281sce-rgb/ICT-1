13fdefbe91381017e20d36ed0964006d7d76c96a
unknown <08250281.sce@rub.edu.bt>
2026-03-20 15:42:49 +0600

    example

---
+++ demo/and or not.py
@@ -0,0 +1,10 @@
+x=True
+y=False
+result_and=x and y # False (both x and y must be True for the result to be True)
+print("Result of AND operation:", result_and)
+
+result_or=x or y # True (at least one of x or y must be True for the result to be True)
+print("logical OR:", result_or)
+
+result_not= not x # False (the NOT operator negates the value of x)
+print("logical NOT:", result_not)   
