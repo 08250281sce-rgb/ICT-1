56ad649a9d10758bcf76f5bfe9c120102eda5da3
unknown <08250281.sce@rub.edu.bt>
2026-03-20 15:05:16 +0600

    solution

--- demo/arithmetic.py
+++ demo/arithmetic.py
@@ -1,8 +1,14 @@
 a=10
 b=5
 addition_result = a + b # 10 + 5 = 15
+print(addition_result)
 subtraction_result = a - b # 10 - 5 = 5
+print(subtraction_result)
 multiplication_result = a * b # 10 * 5 = 50
+print(multiplication_result)
 division_result = a / b  # 10 / 5 = 2.0 (division always returns a float in Python)
+print(division_result)
 exponentiation_result = a ** b # 10 ** 5 = 100000
 modulus_result = a % b # 10 % 5 = 0 (remainder of 10 divided by 5)
+print(exponentiation_result)
+print(modulus_result)
