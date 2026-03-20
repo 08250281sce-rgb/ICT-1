0b99651c1c980a4a4b0847744aba148d0b16ef40
unknown <08250281.sce@rub.edu.bt>
2026-03-20 15:01:49 +0600

    arithmetic function

---
+++ demo/arithmetic.py
@@ -0,0 +1,8 @@
+a=10
+b=5
+addition_result = a + b # 10 + 5 = 15
+subtraction_result = a - b # 10 - 5 = 5
+multiplication_result = a * b # 10 * 5 = 50
+division_result = a / b  # 10 / 5 = 2.0 (division always returns a float in Python)
+exponentiation_result = a ** b # 10 ** 5 = 100000
+modulus_result = a % b # 10 % 5 = 0 (remainder of 10 divided by 5)
