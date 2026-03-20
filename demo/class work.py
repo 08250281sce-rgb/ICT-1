a0a5ae12f6dd0c59c91317cdd2bffc337272781d
unknown <08250281.sce@rub.edu.bt>
2026-03-20 16:20:23 +0600

    class work

---
+++ demo/class work.py
@@ -0,0 +1,13 @@
+name = str(input("what is your name: "))
+print("Hello have a good day, ", name)
+num1= float(input("Enter a number 1: "))
+num2 = float(input("Enter a number 2: "))
+addition_result = num1 + num2
+subtration_result = num1 - num2
+multiply_result = num1 * num2
+division_result = num1 / num2
+print("Addition:", addition_result)
+print("Subtraction:", subtration_result)
+print("Multiplication:", multiply_result)
+print("Division:", division_result)
+print("nice to meet you ", name, "have a good day")
