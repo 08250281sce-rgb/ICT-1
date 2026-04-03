60793f87101e91ccfc861618b02a3bfe6e0746d9
unknown <08250281.sce@rub.edu.bt>
2026-04-03 16:19:52 +0600

    good job

--- demo/desicion.py/%_operator.py
+++ demo/desicion.py/%_operator.py
@@ -1,4 +1,5 @@
 print("currently,we are having %s class" %"PRG101")
 subject= "python"
 module_no= 2.555555555
-print("the module number of %s is %.2f" %(subject,module_no))
+print("the module number of %s is %d" %(subject,module_no))
+print("{s} is a good boy who can be a good programmer but sometime {s} is bad in coding".format(s="sangay"))
--- demo/desicion.py/desision.py
+++ demo/desicion.py/desision.py
@@ -2,7 +2,7 @@ marks1=float(input("enter the marks for first subject:"))
 marks2=float(input("enter the marks for second subject:"))
 marks3=float(input("enter the marks for third subject:"))
 average=(marks1+marks2+marks3)/3
-print ("average marks: ", average)
+print ( "Average is %.2f" %average)
 if (average >= 90 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
     print("Grade: A")
 elif (average >= 80 and marks1 >= 50 and marks2 >= 50 and marks3 >= 50):
